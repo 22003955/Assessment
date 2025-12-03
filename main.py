86d5eff72371502d259bd5d46e44c28e1bddf725
22003955 <22003955@gateshead.ac.uk>
2025-12-03 12:29:44 +0000

    updated main.py with code that classifies temperatures

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print("hello")# this is a comment 
-print("testing")
+temp=int(input("Please input the temperature here:"))
+
+if temp<10:
+  print("Cold")
+
+elif temp<20:
+  print("Warm")
+
+else:
+  print("Hot")
