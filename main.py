7f98e68aacb2a321ad9328f992298c2a4a2319cd
22003955 <22003955@gateshead.ac.uk>
2025-12-04 14:44:26 +0000

    updated main.py by adding define function 'def'

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 temp=int(input("Please input the temperature here: "))
+
 forecast={
-    'Monday': [temp<10],
-    'Tuesday': [temp<20],
-    'Wednesday':[temp>20],
-    'Thursday': [temp<10],
-    'Friday': [temp<20],
-    'Saturday': [temp>20]
+    'Monday': temp < 10,
+    'Tuesday': temp > 10 < 20,
+    'Wednesday': temp > 20 < 25,
+    'Thursday': temp < 10,
+    'Friday': temp < 20,
+    'Saturday': temp > 20,
+    'Sunday': temp > 0
 }
 
-if temp<10:
-    print("Cold")
-
-elif temp<20:
-    print("Warm")
-
-elif temp>20:
-    print("Hot")
+def classify_temperature(temp):
+    if temp<10:
+        print("Cold")
+    elif temp<20:
+        print("Warm")
+    elif temp>20:
+        print("Hot")
 
 for key, value in forecast.items():
     print(f'Today is {key} and it is {value}!')
