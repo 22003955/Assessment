b7fe4de47a25fb7431f5f040ff6117ac3c3beedb
22003955 <22003955@gateshead.ac.uk>
2025-12-04 11:27:36 +0000

    updated main.py by adding a forecast based on given input

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-temp=int(input("Please input the temperature here:"))
+temp=int(input("Please input the temperature here: "))
+forecast={
+    'Monday': [temp<10],
+    'Tuesday': [temp<20],
+    'Wednesday':[temp>20],
+    'Thursday': [temp<10],
+    'Friday': [temp<20],
+    'Saturday': [temp>20]
+}
 
 if temp<10:
-  print("Cold")
+    print("Cold")
 
 elif temp<20:
-  print("Warm")
+    print("Warm")
 
-else:
-  print("Hot")
+elif temp>20:
+    print("Hot")
+
+for key, value in forecast.items():
+    print(f'Today is {key} and it is {value}!')
