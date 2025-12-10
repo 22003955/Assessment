2ab7725f8ec3836725939e5a90eb9c39aab8945a
22003955 <22003955@gateshead.ac.uk>
2025-12-10 12:26:02 +0000

    updated main.py by adding a while loop

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ") 
-temp = float(input("Enter temperature here: ")) 
-
-if unit == "C":
-    temp = round((9 * temp) / 5 + 32, 1)
-    print(f"The temperature in Fahrenheit is: {temp} °F")
-elif unit == "F":
-    temp = round((temp - 32) * 5 / 9, 1)
-    print(f"The temperature in Celsius is {temp} °C")
-else:
-    print(f"{unit} is an invalid unit of measurement.") 
-
 def fahrenheit_to_celsius(fahrenheit):
   celsius = (fahrenheit - 32) * 5/9
   return round(celsius, 2)
@@ -21,3 +9,32 @@ def celsius_to_fahrenheit(celsius):
   return round(fahrenheit, 2)
   
 print(celsius_to_fahrenheit(100))
+
+
+unit = input("Is this temperature in Celsius or Farenheit? (C/F): ") 
+temp = float(input("Enter temperature here: ")) 
+
+if unit == "C":
+    temp = round((9 * temp) / 5 + 32, 1)
+    print(f"The temperature in Farenheit is: {temp} °F")
+elif unit == "F":
+    temp = round((temp - 32) * 5 / 9, 1)
+    print(f"The temperature in Celsius is {temp} °C")
+else:
+    print(f"{unit} is an invalid unit of measurement.") 
+
+
+print("This is a loop for Temperature Conversion.")
+
+while true:
+  choice = input("1 for 2 quit")
+  
+  if choice == 1:
+    1 
+    
+    
+  elif choice ==2:
+    break
+  
+  else:
+    print("INVALID CHOICE!")
