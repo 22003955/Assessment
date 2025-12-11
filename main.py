e521cab89eac8d9eb0a58ad094758312ea66a094
22003955 <22003955@gateshead.ac.uk>
2025-12-11 11:01:25 +0000

    updated main.py by adding a while loop
    The while loop adds the choice to loop through the temperature converter.

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 def fahrenheit_to_celsius(fahrenheit):
-  celsius = (fahrenheit - 32) * 5/9
-  return round(celsius, 2)
-  
+    celsius = (fahrenheit - 32) * 5 / 9
+    return round(celsius, 2)
+
+
 print(fahrenheit_to_celsius(100))
 
+
 def celsius_to_fahrenheit(celsius):
-  fahrenheit = (celsius * 9/5) +32
-  return round(fahrenheit, 2)
-  
+    fahrenheit = (celsius * 9 / 5) + 32
+    return round(fahrenheit, 2)
+
+
 print(celsius_to_fahrenheit(100))
 
-
-unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ") 
-temp = float(input("Enter temperature here: ")) 
+unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+temp = float(input("Enter temperature here: "))
 
 if unit == "C":
     temp = round((9 * temp) / 5 + 32, 1)
@@ -21,20 +23,26 @@ elif unit == "F":
     temp = round((temp - 32) * 5 / 9, 1)
     print(f"The temperature in Celsius is {temp} °C")
 else:
-    print(f"{unit} is an invalid unit of measurement.") 
-
+    print(f"{unit} is an invalid unit of measurement.")
 
 print("This is a loop for Temperature Conversion.")
 
-while true:
-  choice = input("1 for 2 quit")
-  
-  if choice == 1:
-    1 
-    
-    
-  elif choice ==2:
-    break
-  
-  else:
-    print("INVALID CHOICE!")
+while True:
+    choice = int(input("1 for 2 quit"))
+
+    if choice == 1:
+        unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+        temp = float(input("Enter temperature here: "))
+
+        if unit == "C":
+            temp = round((9 * temp) / 5 + 32, 1)
+            print(f"The temperature in Fahrenheit is: {temp} °F")
+        elif unit == "F":
+            temp = round((temp - 32) * 5 / 9, 1)
+            print(f"The temperature in Celsius is {temp} °C")
+        else:
+            print(f"{unit} is an invalid unit of measurement.")
+    elif choice == 2:
+        break
+    else:
+        print("INVALID CHOICE!")
